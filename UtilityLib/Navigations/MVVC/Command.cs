c6ef0a1f39861588883feebd5b07ec92aaa9f3f3
aoton0029 <aotonnnnn@gmail.com>
2024-08-03 23:26:09 +0900

    upd

--- UtilityLib/Navigations/MVVC/Command.cs
+++ UtilityLib/Navigations/MVVC/Command.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UtilityLib.Navigations
+namespace UtilityLib.Navigations.MVVC
 {
     public enum HistoricalNavigationMode
     {
