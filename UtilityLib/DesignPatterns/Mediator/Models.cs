0e7cd779cb00146adc2e3fa86211b4d19425b804
aoton0029 <aotonnnnn@gmail.com>
2024-08-07 23:22:03 +0900

    upd

---
+++ UtilityLib/DesignPatterns/Mediator/Models.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLib.DesignPatterns.Mediator
+{
+    abstract class Mediator
+    {
+        public abstract void Send(string message, Colleague colleague);
+    }
+
+    class ManagerMediator : Mediator
+    {
+        public Colleague Customer { get; set; }
+        public Colleague Programmer { get; set; }
+        public Colleague Tester { get; set; }
+
+        public override void Send(string message, Colleague colleague)
+        {
+            if (colleague == Customer)
+            {
+                Programmer.Notify(message);
+            }
+            else if (colleague == Programmer)
+            {
+                Tester.Notify(message);
+            }
+            else Customer.Notify(message);
+        }
+    }
+
+    abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator mediator) => this.mediator = mediator;
+
+        public virtual void Send(string message) => this.mediator.Send(message, this);
+
+        public abstract void Notify(string message);
+
+    }
+
+    class Customer : Colleague
+    {
+        public Customer(Mediator mediator) : base(mediator) { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine($"Message to customer: {message}");
+        }
+    }
+
+    class Programmer : Colleague
+    {
+        public Programmer(Mediator mediator) : base(mediator) { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine($"Message to programmer: {message}");
+        }
+    }
+
+    class Tester : Colleague
+    {
+        public Tester(Mediator mediator) : base(mediator) { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine($"Message to tester: {message}");
+        }
+    }
+}
---
+++ UtilityLib/DesignPatterns/Mediator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLib.DesignPatterns.Mediator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mediator = new ManagerMediator();
+            var customer = new Customer(mediator);
+            var programmer = new Programmer(mediator);
+            var tester = new Tester(mediator);
+            mediator.Customer = customer;
+            mediator.Programmer = programmer;
+            mediator.Tester = tester;
+            customer.Send("We have an order, need to make a program");
+            programmer.Send("I have done program, need to test it");
+            tester.Send("I have done testing, here is ready program for you");
+        }
+    }
+}
