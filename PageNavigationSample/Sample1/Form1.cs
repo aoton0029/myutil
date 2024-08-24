9ba16d71bf71efc63c3dfa7c8924911737b291a5
aoton0029 <aotonnnnn@gmail.com>
2024-08-24 12:47:20 +0900

    upd

--- PageNavigationSample/Sample1/Form1.cs
+++ PageNavigationSample/Sample1/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static PageNavigationSample.Sample1.NavigationService;
 
 namespace PageNavigationSample.Sample1
 {
