fb3a00509dbcba9a304c4f3774c3c2c7c0bf7968
aoton0029 <aotonnnnn@gmail.com>
2024-08-25 23:52:44 +0900

    upd

---
+++ UtilityLib/Input/TouchForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UtilityLib.Input
+{
+    public partial class TouchForm : Form
+    {
+        // Constants
+        private const int WM_TOUCH = 0x0240;
+        private const int TOUCHEVENTF_MOVE = 0x0001;
+        private const int TOUCHEVENTF_DOWN = 0x0002;
+        private const int TOUCHEVENTF_UP = 0x0004;
+
+        // Structures
+        [StructLayout(LayoutKind.Sequential)]
+        private struct TOUCHINPUT
+        {
+            public int x;
+            public int y;
+            public IntPtr hSource;
+            public int dwID;
+            public int dwFlags;
+            public int dwMask;
+            public int dwTime;
+            public IntPtr dwExtraInfo;
+            public int cxContact;
+            public int cyContact;
+        }
+
+        // DLL Imports
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool RegisterTouchWindow(IntPtr hWnd, uint ulFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool GetTouchInputInfo(IntPtr hTouchInput, int cInputs, [In, Out] TOUCHINPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void CloseTouchInputHandle(IntPtr lParam);
+
+        public TouchForm()
+        {
+            this.Load += new EventHandler(TouchForm_Load);
+        }
+
+        private void TouchForm_Load(object sender, EventArgs e)
+        {
+            // Register the window to receive touch events
+            RegisterTouchWindow(this.Handle, 0);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_TOUCH)
+            {
+                int inputCount = m.WParam.ToInt32();
+                TOUCHINPUT[] inputs = new TOUCHINPUT[inputCount];
+
+                if (GetTouchInputInfo(m.LParam, inputCount, inputs, Marshal.SizeOf(typeof(TOUCHINPUT))))
+                {
+                    for (int i = 0; i < inputCount; i++)
+                    {
+                        TOUCHINPUT ti = inputs[i];
+
+                        // Convert the coordinates to pixels
+                        int x = ti.x / 100;
+                        int y = ti.y / 100;
+
+                        // Handle different touch events
+                        if ((ti.dwFlags & TOUCHEVENTF_DOWN) != 0)
+                        {
+                            OnTouchDown(x, y);
+                        }
+                        else if ((ti.dwFlags & TOUCHEVENTF_MOVE) != 0)
+                        {
+                            OnTouchMove(x, y);
+                        }
+                        else if ((ti.dwFlags & TOUCHEVENTF_UP) != 0)
+                        {
+                            OnTouchUp(x, y);
+                        }
+                    }
+
+                    CloseTouchInputHandle(m.LParam);
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void OnTouchDown(int x, int y)
+        {
+            // タッチダウンイベントの処理
+            Console.WriteLine($"Touch down at ({x}, {y})");
+        }
+
+        private void OnTouchMove(int x, int y)
+        {
+            // タッチムーブイベントの処理
+            Console.WriteLine($"Touch move at ({x}, {y})");
+        }
+
+        private void OnTouchUp(int x, int y)
+        {
+            // タッチアップイベントの処理
+            Console.WriteLine($"Touch up at ({x}, {y})");
+        }
+    }
+}
---
+++ UtilityLib/Win32Methods.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLib
+{
+    internal class Win32Methods
+    {
+        private const int SW_MAXIMIZE = 3; // ウィンドウを最大化するための定数
+
+        [DllImport("user32.dll")]
+        private static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, uint nFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PeekMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax, uint wRemoveMsg);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetWindowText(IntPtr hWnd, string lpString);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetClassName(IntPtr hWnd, System.Text.StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool DestroyWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool CloseWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool ClipCursor(ref RECT lpRect);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern short GetKeyState(int nVirtKey);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetCapture(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // Additional structs used by the methods above
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        public struct MSG
+        {
+            public IntPtr hwnd;
+            public uint message;
+            public IntPtr wParam;
+            public IntPtr lParam;
+            public uint time;
+            public POINT pt;
+        }
+    }
+}
