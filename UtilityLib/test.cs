9a47001bf6a0b43720871483faff8f67243447b0
aoton0029 <38271764+aoton0029@users.noreply.github.com>
2024-09-11 08:46:55 +0900

    Update test.cs

--- UtilityLib/test.cs
+++ UtilityLib/test.cs
@@ -89,3 +89,53 @@ public static class ObservableExtensions
         });
     }
 }
+
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public static class ObservableExtensions
+{
+    // ObserveOn: 通知を特定のスケジューラ（スレッド）上で行う
+    public static IObservable<T> ObserveOn<T>(this IObservable<T> source, SynchronizationContext context)
+    {
+        return new AnonymousObservable<T>(observer =>
+        {
+            return source.Subscribe(
+                onNext: value =>
+                {
+                    context.Post(_ => observer.OnNext(value), null);
+                },
+                onError: ex =>
+                {
+                    context.Post(_ => observer.OnError(ex), null);
+                },
+                onCompleted: () =>
+                {
+                    context.Post(_ => observer.OnCompleted(), null);
+                });
+        });
+    }
+
+    // 別スレッドで実行するObserveOn（タスクで処理）
+    public static IObservable<T> ObserveOn<T>(this IObservable<T> source, TaskScheduler scheduler)
+    {
+        return new AnonymousObservable<T>(observer =>
+        {
+            return source.Subscribe(
+                onNext: value =>
+                {
+                    Task.Factory.StartNew(() => observer.OnNext(value), CancellationToken.None, TaskCreationOptions.None, scheduler);
+                },
+                onError: ex =>
+                {
+                    Task.Factory.StartNew(() => observer.OnError(ex), CancellationToken.None, TaskCreationOptions.None, scheduler);
+                },
+                onCompleted: () =>
+                {
+                    Task.Factory.StartNew(() => observer.OnCompleted(), CancellationToken.None, TaskCreationOptions.None, scheduler);
+                });
+        });
+    }
+}
