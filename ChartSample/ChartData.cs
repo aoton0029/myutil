5aa56a941f5a0088868d81fab600457fced8913d
aoton0029 <aotonnnnn@gmail.com>
2024-09-14 00:05:10 +0900

    upd

---
+++ ChartSample/ChartData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartSample
+{
+    public class ChartData
+    {
+        public double Period { get; set; }  // 周期 (秒)
+        public double Frequency { get; set; } // 周波数 (Hz)
+        public double Amplitude { get; set; } // 振幅 (V)
+        public double StartTime { get; set; } // 開始秒数
+        public double EndTime { get; set; }   // 終了秒数
+
+        // コンストラクタ
+        public ChartData(double period, double frequency, double amplitude, double startTime, double endTime)
+        {
+            Period = period;
+            Frequency = frequency;
+            Amplitude = amplitude;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        // 周期に基づいて振幅を計算する関数
+        public double CalculateAmplitude(double time)
+        {
+            // 正弦波の例: A * sin(2 * π * f * t)
+            return Amplitude * Math.Sin(2 * Math.PI * Frequency * time);
+        }
+    }
+}
---
+++ ChartSample/ChartDataManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartSample
+{
+    public class ChartDataManager
+    {
+        public List<ChartData> DataList { get; set; } = new List<ChartData>();
+
+        // チャートデータを追加
+        public void AddData(ChartData data)
+        {
+            DataList.Add(data);
+        }
+
+        // 指定した時刻における振幅のリストを取得
+        public List<double> GetAmplitudes(double time)
+        {
+            var amplitudes = new List<double>();
+            foreach (var data in DataList)
+            {
+                if (time >= data.StartTime && time <= data.EndTime)
+                {
+                    amplitudes.Add(data.CalculateAmplitude(time));
+                }
+            }
+            return amplitudes;
+        }
+
+        // 指定した範囲の時刻に対する全てのデータポイントを取得
+        public List<(double Time, double Amplitude)> GetPlotData(double startTime, double endTime, double step)
+        {
+            var plotData = new List<(double Time, double Amplitude)>();
+            for (double time = startTime; time <= endTime; time += step)
+            {
+                var amplitudes = GetAmplitudes(time);
+                foreach (var amplitude in amplitudes)
+                {
+                    plotData.Add((time, amplitude));
+                }
+            }
+            return plotData;
+        }
+    }
+}
