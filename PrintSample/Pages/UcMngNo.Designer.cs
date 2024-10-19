cdd947bb57e78adb20ae28e55b7049d39a82e69d
aoton0029 <aotonnnnn@gmail.com>
2024-10-19 15:43:49 +0900

    upd

---
+++ PrintSample/Pages/UcMngNo.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace PrintSample.Pages
+{
+    partial class UcMngNo
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button7 = new Button();
+            label5 = new Label();
+            button1 = new Button();
+            panel3 = new Panel();
+            button5 = new Button();
+            button6 = new Button();
+            label4 = new Label();
+            button4 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            button3 = new Button();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            panel2 = new Panel();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel1.BackColor = SystemColors.ControlDarkDark;
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 40);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(386, 641);
+            panel1.TabIndex = 0;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.RoyalBlue;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("MS UI Gothic", 24F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            button7.ForeColor = Color.White;
+            button7.Location = new Point(314, 22);
+            button7.Margin = new Padding(0);
+            button7.Name = "button7";
+            button7.Size = new Size(50, 60);
+            button7.TabIndex = 15;
+            button7.Text = "▶";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.White;
+            label5.Font = new Font("Yu Gothic UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(70, 22);
+            label5.Name = "label5";
+            label5.Size = new Size(241, 60);
+            label5.TabIndex = 14;
+            label5.Text = "自由入力";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.RoyalBlue;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("MS UI Gothic", 24F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(17, 22);
+            button1.Margin = new Padding(0);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 60);
+            button1.TabIndex = 13;
+            button1.Text = "◀";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button5);
+            panel3.Controls.Add(button6);
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(label3);
+            panel3.Location = new Point(17, 246);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(347, 200);
+            panel3.TabIndex = 2;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.White;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("MS UI Gothic", 15.75F);
+            button5.Location = new Point(149, 20);
+            button5.Name = "button5";
+            button5.Size = new Size(112, 69);
+            button5.TabIndex = 12;
+            button5.Text = "中";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.White;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("MS UI Gothic", 15.75F);
+            button6.Location = new Point(31, 20);
+            button6.Name = "button6";
+            button6.Size = new Size(112, 69);
+            button6.TabIndex = 11;
+            button6.Text = "クリアラベル";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Yu Gothic UI", 14.25F);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(17, 115);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 25);
+            label4.TabIndex = 10;
+            label4.Text = "QR";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.White;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("MS UI Gothic", 21.75F);
+            button4.Location = new Point(195, 143);
+            button4.Name = "button4";
+            button4.Size = new Size(169, 69);
+            button4.TabIndex = 9;
+            button4.Text = "なし";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("MS UI Gothic", 21.75F);
+            button2.Location = new Point(17, 143);
+            button2.Name = "button2";
+            button2.Size = new Size(169, 69);
+            button2.TabIndex = 8;
+            button2.Text = "あり";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Yu Gothic UI", 14.25F);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(16, 112);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 25);
+            label3.TabIndex = 7;
+            label3.Text = "枚数";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.White;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            button3.Location = new Point(17, 553);
+            button3.Name = "button3";
+            button3.Size = new Size(347, 74);
+            button3.TabIndex = 6;
+            button3.Text = "印刷";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FlatStyle = FlatStyle.Flat;
+            comboBox1.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(17, 502);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(347, 33);
+            comboBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            textBox1.Location = new Point(21, 139);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(143, 33);
+            textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 14.25F);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 474);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 25);
+            label1.TabIndex = 2;
+            label1.Text = "印刷場所";
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.BackColor = Color.LightSlateGray;
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1184, 40);
+            panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Yu Gothic UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(15, 5);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 30);
+            label2.TabIndex = 7;
+            label2.Text = "label2";
+            // 
+            // UcMngNo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "UcMngNo";
+            Size = new Size(1184, 681);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button3;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label3;
+        private Button button4;
+        private Button button2;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Button button7;
+        private Panel panel3;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ PrintSample/UcPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintSample
+{
+    public partial class UcPage : UserControl
+    {
+        public UcPage()
+        {
+            InitializeComponent();
+        }
+
+        public virtual void OnNavigatedTo(object parameter)
+        {
+            // ページが表示される際にパラメータを受け取る処理を実装
+        }
+
+        public virtual void OnNavigatedFrom()
+        {
+            // ページが離れる際に必要な処理を実装
+        }
+    }
+}
