0c058b3aba8967b2559fa1675521a209fdc244f7
aoton0029 <aotonnnnn@gmail.com>
2024-12-16 22:12:50 +0900

    upd

--- DotNet45/Form1.cs
+++ DotNet45/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,26 +14,63 @@ namespace DotNet45
 {
     public partial class Form1 : Form
     {
+        // Raw Input structures
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWINPUTDEVICE
+        {
+            public ushort UsagePage;
+            public ushort Usage;
+            public uint Flags;
+            public IntPtr Target;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool RegisterRawInputDevices(RAWINPUTDEVICE[] pRawInputDevices, uint uiNumDevices, uint cbSize);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetRawInputData(IntPtr hRawInput, uint uiCommand, IntPtr pData, ref uint pcbSize, uint cbSizeHeader);
+
+        const int RIM_TYPEHID = 2;
+        const int WM_INPUT = 0x00FF;
+
         public Form1()
         {
-            InitializeComponent();
+            // Register Raw Input for touch devices
+            RAWINPUTDEVICE[] rid = new RAWINPUTDEVICE[1];
+            rid[0].UsagePage = 0x0D; // HID_USAGE_PAGE_DIGITIZER
+            rid[0].Usage = 0x04;     // HID_USAGE_DIGITIZER_TOUCH_SCREEN
+            rid[0].Flags = 0x00000000; // RIDEV_INPUTSINK
+            rid[0].Target = Handle;
 
-            //DataTable table = new DataTable();
+            if (!RegisterRawInputDevices(rid, (uint)rid.Length, (uint)Marshal.SizeOf(typeof(RAWINPUTDEVICE))))
+            {
+                throw new Exception("Failed to register raw input device.");
+            }
+        }
 
-            //try
-            //{
-            //    table.BeginLoadData();
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_INPUT)
+            {
+                uint dwSize = 0;
+                GetRawInputData(m.LParam, 0x10000003, IntPtr.Zero, ref dwSize, (uint)Marshal.SizeOf(typeof(RAWINPUTDEVICE)));
 
-            //    for (int i = 0; i < 100000; i++)
-            //    {
-            //        object[] row = { i, $"Name_{i}", 20 + (i % 50) };
-            //        table.LoadDataRow(row, true);
-            //    }
-            //}
-            //finally
-            //{
-            //    table.EndLoadData();
-            //}
+                IntPtr buffer = Marshal.AllocHGlobal((int)dwSize);
+                try
+                {
+                    if (GetRawInputData(m.LParam, 0x10000003, buffer, ref dwSize, (uint)Marshal.SizeOf(typeof(RAWINPUTDEVICE))) == dwSize)
+                    {
+                        // Process touch input data here
+                        Debug.WriteLine("Touch event detected!");
+                    }
+                }
+                finally
+                {
+                    Marshal.FreeHGlobal(buffer);
+                }
+            }
+
+            base.WndProc(ref m);
         }
     }
 }
---
+++ UtilityLib/Input/RawInputHandler.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLib.Input
+{
+    public class RawInputHandler : IDisposable
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWINPUTDEVICE
+        {
+            public ushort UsagePage;
+            public ushort Usage;
+            public uint Flags;
+            public IntPtr Target;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWINPUTHEADER
+        {
+            public uint Type;
+            public uint Size;
+            public IntPtr Device;
+            public IntPtr wParam;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct RAWINPUT
+        {
+            [FieldOffset(0)]
+            public RAWINPUTHEADER Header;
+        }
+
+        private const int RIM_TYPEHID = 2;
+        private const int WM_INPUT = 0x00FF;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool RegisterRawInputDevices(RAWINPUTDEVICE[] pRawInputDevices, uint uiNumDevices, uint cbSize);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetRawInputData(IntPtr hRawInput, uint uiCommand, IntPtr pData, ref uint pcbSize, uint cbSizeHeader);
+
+        public event Action<string> OnRawInputReceived;
+
+        public RawInputHandler(IntPtr windowHandle)
+        {
+            RegisterDevices(windowHandle);
+        }
+
+        private void RegisterDevices(IntPtr windowHandle)
+        {
+            RAWINPUTDEVICE[] devices = new RAWINPUTDEVICE[1];
+            devices[0].UsagePage = 0x0D; // HID_USAGE_PAGE_DIGITIZER
+            devices[0].Usage = 0x04;     // HID_USAGE_DIGITIZER_TOUCH_SCREEN
+            devices[0].Flags = 0x00000000; // No special flags
+            devices[0].Target = windowHandle;
+
+            if (!RegisterRawInputDevices(devices, (uint)devices.Length, (uint)Marshal.SizeOf(typeof(RAWINPUTDEVICE))))
+            {
+                throw new Exception("Failed to register raw input devices.");
+            }
+        }
+
+        public void ProcessMessage(IntPtr lParam)
+        {
+            uint size = 0;
+            GetRawInputData(lParam, 0x10000003, IntPtr.Zero, ref size, (uint)Marshal.SizeOf(typeof(RAWINPUTHEADER)));
+
+            IntPtr buffer = Marshal.AllocHGlobal((int)size);
+            try
+            {
+                if (GetRawInputData(lParam, 0x10000003, buffer, ref size, (uint)Marshal.SizeOf(typeof(RAWINPUTHEADER))) == size)
+                {
+                    RAWINPUT rawInput = Marshal.PtrToStructure<RAWINPUT>(buffer);
+                    ProcessRawInput(rawInput);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+
+        private void ProcessRawInput(RAWINPUT rawInput)
+        {
+            if (rawInput.Header.Type == RIM_TYPEHID)
+            {
+                OnRawInputReceived?.Invoke("Touch event detected!");
+            }
+        }
+
+        public void Dispose()
+        {
+            // No resources to release
+        }
+    }
+
+    public class MainForm : Form
+    {
+        private RawInputHandler _rawInputHandler;
+
+        public MainForm()
+        {
+            Text = "Raw Input Monitor";
+            Width = 800;
+            Height = 600;
+
+            // RawInputHandlerの初期化
+            _rawInputHandler = new RawInputHandler(this.Handle);
+            _rawInputHandler.OnRawInputReceived += HandleRawInput;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_INPUT = 0x00FF;
+
+            if (m.Msg == WM_INPUT)
+            {
+                // RawInputHandlerにメッセージを渡す
+                _rawInputHandler.ProcessMessage(m.LParam);
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void HandleRawInput(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            _rawInputHandler.Dispose();
+        }
+    }
+}
