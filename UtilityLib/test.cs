331e81fb0ec2e78a10642c902d0fc53efc05c0b0
aoton0029 <38271764+aoton0029@users.noreply.github.com>
2024-12-20 08:42:50 +0900

    test.cs の更新

--- UtilityLib/test.cs
+++ UtilityLib/test.cs
@@ -1,3 +1,73 @@
+DECLARE @cols NVARCHAR(MAX), @query NVARCHAR(MAX);
+
+-- 動的に列名を生成
+SELECT @cols = STRING_AGG(
+    QUOTENAME(CONCAT('Quantity_Option', OptionID)) + ',' + 
+    QUOTENAME(CONCAT('Amount_Option', OptionID)), 
+    ','
+)
+FROM (
+    SELECT DISTINCT OptionID 
+    FROM PartsTable
+) AS DistinctOptions;
+
+-- 動的SQLクエリの生成
+SET @query = '
+SELECT PartNumber, ' + @cols + '
+FROM (
+    SELECT 
+        PartNumber, 
+        CONCAT(''Quantity_Option'', OptionID) AS QuantityColumn, 
+        CONCAT(''Amount_Option'', OptionID) AS AmountColumn,
+        Quantity,
+        Amount
+    FROM PartsTable
+) AS SourceTable
+PIVOT (
+    MAX(Quantity) FOR QuantityColumn IN (' + STRING_AGG(QUOTENAME(CONCAT('Quantity_Option', OptionID)), ',') + ')
+) AS PivotQuantity
+PIVOT (
+    MAX(Amount) FOR AmountColumn IN (' + STRING_AGG(QUOTENAME(CONCAT('Amount_Option', OptionID)), ',') + ')
+) AS PivotAmount
+ORDER BY PartNumber;
+';
+
+-- クエリの実行
+EXEC sp_executesql @query;
+
+
+
+
+-- ステップ1: ユニークなオプション名を取得
+DECLARE @columns NVARCHAR(MAX);
+DECLARE @sql NVARCHAR(MAX);
+
+-- カンマ区切りのオプションリストを生成
+SELECT @columns = STRING_AGG(QUOTENAME(option), ',') 
+FROM (SELECT DISTINCT option FROM parts) AS options;
+
+-- ステップ2: 動的SQLの作成
+SET @sql = '
+SELECT
+    part_number, ' + @columns + '
+FROM
+(
+    SELECT 
+        part_number, 
+        option, 
+        quantity
+    FROM parts
+) AS source_table
+PIVOT
+(
+    MAX(quantity)
+    FOR option IN (' + @columns + ')
+) AS pivot_table
+ORDER BY part_number;';
+
+-- ステップ3: 実行
+EXEC sp_executesql @sql;
+
 using System;
 using System.Collections.Generic;
 
