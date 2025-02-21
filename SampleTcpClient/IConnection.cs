aea13eccd0f0d80a825bb60e15bd613f0036ed0d
aoton0029 <aotonnnnn@gmail.com>
2025-02-21 23:00:06 +0900

    upd

---
+++ SampleTcpClient/IConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTcpClient
+{
+    public interface IConnection : IDisposable
+    {
+        bool IsConnected { get; }
+        void Connect();
+        void Disconnect();
+        void Send(string command);
+        string Read();
+    }
+}
